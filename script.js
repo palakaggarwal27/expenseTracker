4f807536aceafcce0a4b0693e5b726141295a684
PalakAggarwal27 <palakaggarwal2712007@gmail.com>
2025-11-24 12:38:52 +0530

    added basicadd_expense logic

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const modal = document.getElementById('backdrop');
 const modalContent = document.getElementById('monthly_budget');
 const setBudgetBtn = document.getElementById('set_budget');
+const addExpenseBtn = document.getElementById('add_expense');
+const add_date = document.getElementById('date').value;
+const add_amount = document.getElementById('amount').value;
+const add_info = document.getElementById('info').value;
+const add_category = document.getElementById('category').value;
+
+
 
 
 setBudgetBtn.addEventListener('click', () => {
     const budgetInput = document.getElementById('budget_input').value;
     if(budgetInput && budgetInput > 0){
-        // Here you can add code to save the budget value
         console.log(`Monthly Budget Set: Rs. ${budgetInput}`);
         closeModal();
     }
@@ -25,3 +31,15 @@ function openModal() {
 }
 
 // setTimeout(openModal, 500);
+
+addExpenseBtn.addEventListener('click', add_expense());
+
+function add_expense(){
+    if (add_date && add_amount && add_info && add_category){
+        console.log(`Expense Added: ${add_date}, Rs.${add_amount}, ${add_info}, ${add_category}`);
+    }
+
+    else {
+        console.log("Please fill all the fields to add an expense.");
+    }
+}
